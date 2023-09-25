696fbf6ab90defbb146d562962aeec4da5466873
ewudes <ewudes@gmail.com>
2023-09-25 14:29:27 +0300

    add: login component

---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+    <form class="auth">
+      <h2 class="auth-title">Авторизуйтесь</h2>
+      <label for="login" class="auth-label">Логин</label>
+      <input
+        type="text"
+        id="login"
+        class="auth-login"
+        placeholder="Введите логин"
+      />
+      <label for="password" class="auth-label"></label>
+      <input
+        type="text"
+        id="password"
+        class="auth-password"
+        placeholder="Введите пароль"
+      />
+      <button type="submit">Войти</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
+<style>
+
+</style>
